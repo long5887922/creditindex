5e0701a90d0e003bb97617da93934ec89109c10a
“zJohny” <2312006169@qq.com>
2017-10-18 16:19:43 +0800

    整合

--- src/main/java/com/zy/creditindex/controller/ControllerUser.java
+++ src/main/java/com/zy/creditindex/controller/ControllerUser.java
@@ -1,13 +1,21 @@
 package com.zy.creditindex.controller;
 
-import com.zy.creditindex.entity.AdminUser;
+import com.zy.creditindex.aspect.HttpAspect;
+import com.zy.creditindex.entity.User;
 import com.zy.creditindex.repostory.UserRepostory;
+import com.zy.creditindex.service.AdminUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 /**
  * Created by ${ZhaoYing}on 2017/9/28 0028
@@ -15,20 +23,74 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/user")
 public class ControllerUser {
+    private final static Logger logger= LoggerFactory.getLogger(HttpAspect.class);
+    @Autowired
+   private UserRepostory userrepostory;
 
     @Autowired
-    private UserRepostory userrepostory;
+    private AdminUserService adminUserService;
 
+    @PostMapping("/byName")
+    public User selectName(String name){
+        return userrepostory.findByName(name);
+    }
+    @PostMapping("queryByName")
+    public User queryByName(String name){
+        return adminUserService.querUsernumber(name);
+    }
     /**
      * 管理用户注册方法
-     * @param adminser
+     * @param
      * @return
      */
     @PostMapping("/save")
-    public AdminUser SaveUser(@Valid AdminUser adminser ){
-        adminser.setName(adminser.getName());
-        adminser.setUsernumber(adminser.getUsernumber());
-        return userrepostory.save(adminser);
+    public User SaveUser(@Valid User user ){
+        user.setName(user.getName());
+        user.setPassword(user.getPassword());
+        return userrepostory.save(user);
+    }
+
+    /**
+     * ID查询用户信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/finduser/{id}")
+    public User getUser(@PathVariable("id")String id){
+        return userrepostory.findOne(id);
+    }
+
+    @GetMapping("/finduser/number/{number}")
+    public User getByUsernumber(@PathVariable("number") String usernumber){
+        return adminUserService.querUsernumber(usernumber);
+    }
+
+    @PostMapping("/controller")
+    public String loginController(@RequestParam("usernumber")String usernumber,@RequestParam("password")String password){
+        logger.info("--------------------------AJAX登录请求-----------------------------");
+        logger.info(usernumber);
+        logger.info(password);
+        HashMap<String, Object> map = new HashMap<String, Object>();//传递异常信息
+        try {
+            Subject subject = SecurityUtils.getSubject();
+            UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(usernumber,password);
+            subject.login(usernamePasswordToken);
+            System.out.println(subject.isAuthenticated());
+            map.put("success", true);
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            map.put("success", false);
+            map.put("message", "用户名不存在···");
+        }catch (IncorrectCredentialsException e) {
+            map.put("success", false);
+            map.put("message", "密码错误···");
+        }catch (RuntimeException e) {
+            map.put("success", false);
+            map.put("message", e.getMessage());
+        }
+//        GsonUtil.writeToResponse(map);
+
+        return "frame";
     }
 
 }
