2c726e438f21aaa38b4fa58275177871b35745f2
“zJohny” <2312006169@qq.com>
2017-10-19 09:21:28 +0800

    整合

--- src/main/java/com/zy/creditindex/repostory/UserRepostory.java
+++ src/main/java/com/zy/creditindex/repostory/UserRepostory.java
@@ -1,11 +1,17 @@
 package com.zy.creditindex.repostory;
 
-import com.zy.creditindex.entity.AdminUser;
+import com.zy.creditindex.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * 管理用户的数据持久化
  * Created by ${ZhaoYing}on 2017/9/28 0028
  */
-    public interface UserRepostory extends JpaRepository<AdminUser,Integer> {
+public interface UserRepostory extends JpaRepository<User,String> {
+    /**
+     * 通过名字来查询
+     */
+    public User findByName(String usernumber);
+
+
 }
