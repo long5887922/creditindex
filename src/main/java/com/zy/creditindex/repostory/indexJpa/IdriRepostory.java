fe71bfd57b40ce29fbb780db7490c9206d78c3b5
“zJohny” <2312006169@qq.com>
2017-10-23 17:02:46 +0800

    整合

---
+++ src/main/java/com/zy/creditindex/repostory/indexJpa/IdriRepostory.java
@@ -0,0 +1,11 @@
+package com.zy.creditindex.repostory.indexJpa;
+
+import com.zy.creditindex.entity.idri.idri;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by ${ZhaoYing}on 2017/10/23 0023
+ */
+public interface IdriRepostory extends JpaRepository<idri,String> {
+
+}
