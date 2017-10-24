9ccb0f89621c4341233503ad4284c4e3b796fb30
“zJohny” <2312006169@qq.com>
2017-10-24 18:05:24 +0800

    整合

---
+++ src/main/java/com/zy/creditindex/entity/idri/idri.java
@@ -0,0 +1,66 @@
+package com.zy.creditindex.entity.idri;
+
+import com.zy.creditindex.entity.pk.idriPK;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.math.BigDecimal;
+/**
+ * Created by ${ZhaoYing}on 2017/10/23 0023
+ */
+@Entity
+@IdClass(idriPK.class)
+@Table(name = "idri")
+public class idri implements Serializable {
+    // PRIMARY KEY(indu_code, index_date, weight_type)
+    private String inducode;// indu_code VARCHAR(4),   -- 行业代码
+    private Date indexdate;//index_date DATE,        -- 指数计算日期
+    private String weighttype ; //weight_type VARCHAR(4), -- 加权类型（01：等权；02：债券加权）
+    private Integer corpcount;  //corp_count INT,         -- 公司个数。真正计算在内的公司
+    private BigDecimal idri;//idri DECIMAL(30, 4),    -- 行业信贷违约指数
+
+    @Id
+    @Column(name = "indu_code")
+    public String getInducode() {
+        return inducode;
+    }
+
+    public void setInducode(String inducode) {
+        this.inducode = inducode;
+    }
+    @Id
+    @Column(name = "index_date")
+    public Date getIndexdate() {
+        return indexdate;
+    }
+
+    public void setIndexdate(Date indexdate) {
+        this.indexdate = indexdate;
+    }
+    @Id
+    @Column(name = "weight_type")
+    public String getWeighttype() {
+        return weighttype;
+    }
+
+    public void setWeighttype(String weighttype) {
+        this.weighttype = weighttype;
+    }
+    @Column(name = "corp_count")
+    public Integer getCorpcount() {
+        return corpcount;
+    }
+
+    public void setCorpcount(Integer corpcount) {
+        this.corpcount = corpcount;
+    }
+
+    public BigDecimal getIdri() {
+        return idri;
+    }
+
+    public void setIdri(BigDecimal idri) {
+        this.idri = idri;
+    }
+}
---
+++ src/main/java/com/zy/creditindex/entity/pk/idriPK.java
@@ -0,0 +1,41 @@
+package com.zy.creditindex.entity.pk;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * Created by ${ZhaoYing}on 2017/10/23 0023
+ */
+@Embeddable
+public class idriPK implements Serializable{
+    private String inducode;// indu_code VARCHAR(4),   -- 行业代码
+    private Date indexdate;//index_date DATE,        -- 指数计算日期
+    private String weighttype ; //weight_type VARCHAR(4), -- 加权类型（01：等权；02：债券加权）
+
+    @Column(name = "indu_code")
+    public String getInducode() {
+        return inducode;
+    }
+
+    public void setInducode(String inducode) {
+        this.inducode = inducode;
+    }
+    @Column(name = "index_date")
+    public Date getIndexdate() {
+        return indexdate;
+    }
+
+    public void setIndexdate(Date indexdate) {
+        this.indexdate = indexdate;
+    }
+    @Column(name = "weight_type")
+    public String getWeighttype() {
+        return weighttype;
+    }
+
+    public void setWeighttype(String weighttype) {
+        this.weighttype = weighttype;
+    }
+}
