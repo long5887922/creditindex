ff1515fb19f73af220427451d5accc20190db086
“zJohny” <2312006169@qq.com>
2017-10-31 14:45:22 +0800

    整合

--- src/main/java/com/zy/creditindex/util/DateUtil.java
+++ src/main/java/com/zy/creditindex/util/DateUtil.java
@@ -1,7 +1,10 @@
 package com.zy.creditindex.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
+import static com.zy.creditindex.util.DateTimeUtil.initHolidayList;
 
 /**
  * Created by ${ZhaoYing}on 2017/10/26 0026
@@ -20,5 +23,14 @@ public class DateUtil {
     public static Date endtime(){
         Date endtime=new java.sql.Date(new Date().getTime());
         return endtime;
+    }
+    // 一年前的数据
+    public static Date oneYer(){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();//日历对象
+        calendar.setTime(new java.util.Date());//设置当前日期
+        calendar.add(Calendar.MONTH, -12);//份减6
+        Date starttimeyer = calendar.getTime();
+        return starttimeyer;
     }
 }
