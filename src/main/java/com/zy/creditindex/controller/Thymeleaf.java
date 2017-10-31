7400e0b7c53e72cc6c25c30b0b1bf6e6ff28f4cc
“zJohny” <2312006169@qq.com>
2017-10-31 14:44:19 +0800

    整合

--- src/main/java/com/zy/creditindex/controller/Thymeleaf.java
+++ src/main/java/com/zy/creditindex/controller/Thymeleaf.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -99,6 +100,10 @@ public class Thymeleaf {
 		}
 		model.addAttribute("list",l);
         return  "weightingAndIsobar";
+    }
+    @GetMapping("/test")
+    public String testww(){
+        return "IndustryRanking/IndustryRanking";
     }
 }
 
