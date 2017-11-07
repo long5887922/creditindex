709a68c7edbb1a5452097943326374be7bf612fb
“zJohny” <2312006169@qq.com>
2017-11-07 17:53:31 +0800

    整合

--- src/main/java/com/zy/creditindex/controller/indexandidri/DownLoadAction.java
+++ src/main/java/com/zy/creditindex/controller/indexandidri/DownLoadAction.java
@@ -5,18 +5,13 @@ package com.zy.creditindex.controller.indexandidri;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 
 /**
