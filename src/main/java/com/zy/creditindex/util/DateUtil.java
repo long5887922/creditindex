59563d44732952e3e05a8ecf3bbe03a68b818363
“zJohny” <2312006169@qq.com>
2017-11-09 11:14:06 +0800

    整合

--- src/main/java/com/zy/creditindex/util/DateUtil.java
+++ src/main/java/com/zy/creditindex/util/DateUtil.java
@@ -21,13 +21,15 @@ public class DateUtil {
         return starttime;
     }
     //获取当前系统日期（即昨天的交易，【业务要求】当天的交易还未开始）
-    public static Date endtime(){
+    public static Date endtime()throws Exception {
 //        Date endtime=new java.sql.Date(new Date().getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();//日历对象
-        calendar.setTime(new java.util.Date());//设置当前日期
+        calendar.setTime(new Date());//设置当前日期
         calendar.add(Calendar.DATE, -1);//当天减一 ：昨天
-        Date endtime = calendar.getTime();
-        return endtime;
+        Date endtime =calendar.getTime();
+        String date = format.format(endtime);
+        return format.parse(date);
     }
     // 一年前的数据
     public static Date oneYer(){
